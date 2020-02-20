d0c71992c14a1a5a0c95dc0b98ad1fff456ef831
Luiz Renato <luiz.santos@logusinfo.com.br>
2020-02-20 18:43:27 -0300

    Criação Subject

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,22 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PhotoService } from '../photo/photo.service';
 import { ActivatedRoute } from '@angular/router';
 import { Photo } from '../photo/photo';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos  : Photo[] = [];
   filter : string = '';
-  
+  subject : Subject<string> = new Subject<string>();
+
   constructor(private activactedRoute : ActivatedRoute) { }
 
   ngOnInit() {
     this.photos = this.activactedRoute.snapshot.data.photos;
+    this.subject.subscribe(valor => this.filter = valor);
+  }
+
+  ngOnDestroy(): void {
+    this.subject.unsubscribe();
   }
 
 }
